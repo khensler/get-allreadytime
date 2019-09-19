de6f674077173fa5400c6c69d975880f3f665667
khensler <46349570+khensler@users.noreply.github.com>
2019-09-19 14:29:16 -0400

    Create get-allreadytime.ps1

---
+++ get-allreadytime.ps1
@@ -0,0 +1,10 @@
+Get-Stat -realtime -maxsamples 1 -Entity $entity -Stat $stat -ErrorAction SilentlyContinue | Group-Object -Property {$_.Entity.Name} | %{
+  $_.Group | %{
+    New-Object PSObject -Property @{
+      VM = $_.Entity.Name
+      Date = $_.Timestamp
+      ReadyMs = $_.Value
+      ReadyPerc = "{0:P2}" -f ($_.Value/($_.Intervalsecs*1000))
+    }
+  }
+} | export-csv ready.txt
